d9acf739cb6f68bdfcfa9dcf20469df2215fd6dc
MikolajZielinski <mikolaj.zielinski@doctorate.put.poznan.pl>
2025-06-23 08:55:22 +0000

    Bug fix, added covariances to optimizer

--- gnerf/optimization/optimizers.py
+++ gnerf/optimization/optimizers.py
@@ -21,7 +21,7 @@ def initialize_optimizer(config, radiance_field, weight_decay):
     
     gau_params, codebook_params, rest_params = [], [], []
     for name in params_dict:
-        if ("means" in name) or ("covariances" in name):
+        if ("means" in name) or ("log_diag_cov_2d" in name):
             gau_params.append(params_dict[name])
         elif "feats" in name:
             codebook_params.append(params_dict[name])
